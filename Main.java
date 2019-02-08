35e69cc2debce49e3e180f878ea748678b1c0464
Darktone <42974480+Jos260400@users.noreply.github.com>
2019-02-08 17:37:08 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 // Fernando Jose Garavito Ovando    18071
 // Maria Jose Castro 181202
 // Hoja de trabajo No. 3
-// Hoja_3.java
+// Main.java
 
-//importo random para genrar los numeros
+//Importamos Random para generar los números enteros aleatorios
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        //ciclo para crear los numeros
+        //Ciclo para crear los números
         for (int i=0; i<3001; i++){
-            //imprime
+            //Imprime los 3000 números enteros aleatorios
             System.out.println(random.nextInt(3001));
         }
 
