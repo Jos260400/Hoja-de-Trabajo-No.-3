1c895e891728b94d1af78ab5c8a8099573ca56e3
iconicmajo <42973036+iconicmajo@users.noreply.github.com>
2019-02-08 11:46:37 -0600

    Update and rename Hoja_3.java to Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
-// Fernando José Garavito Ovando    18071
-// María Jose
+// Fernando Jose Garavito Ovando    18071
+// Maria Jose Castro 181202
 // Hoja de trabajo No. 3
 // Hoja_3.java
 
-
+//importo random para genrar los numeros
 import java.util.Random;
-public class Hoja_3 {
+public class Main {
     public static void main(String[] args) {
         Random random = new Random();
+        //ciclo para crear los numeros
         for (int i=0; i<3001; i++){
+            //imprime
             System.out.println(random.nextInt(3001));
         }
 
