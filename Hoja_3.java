1d67b5e236995d6ce71c8da91aafb9e7b9a051e1
Darktone <42974480+Jos260400@users.noreply.github.com>
2019-02-08 08:04:15 -0600

    Add files via upload

---
+++ Hoja_3.java
@@ -0,0 +1,17 @@
+// Fernando José Garavito Ovando    18071
+// María Jose
+// Hoja de trabajo No. 3
+// Hoja_3.java
+
+
+import java.util.Random;
+public class Hoja_3 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i=0; i<3001; i++){
+            System.out.println(random.nextInt(3001));
+        }
+
+    }
+}
+
